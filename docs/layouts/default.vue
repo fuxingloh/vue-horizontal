39448433a33d68e8d77e3845ba47f6d6a158836a
Fuxing Loh <git@fuxing.dev>
2020-12-13 21:56:52 +0800

    Use a simple symbolic link instead of lerna to link SFC

--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -3,7 +3,3 @@
     <Nuxt/>
   </div>
 </template>
-
-<style>
-
-</style>
--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -1,16 +1,39 @@
 <template>
-  <div>
+  <div class="container mx-auto px-4">
+    <h1>Vue</h1>
+
+    <vue-horizontal>
+      <div v-for="item in items" class="item" :key="item.i">
+        <h3>{{ item.title }}</h3>
+        <p>{{ item.content }}</p>
+      </div>
+    </vue-horizontal>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import VueHorizontal from "~/components/vue-horizontal.vue";
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {VueHorizontal},
+  data() {
+    return {
+      items: [...Array(20).keys()].map((i) => {
+        return {i, title: `Item ${i}`, content: `🚀 Simple content ${i}`};
+      }),
+    }
+  }
+})
 </script>
 
 <style scoped>
-.container {
-  /*@apply min-h-screen flex justify-center items-center text-center mx-auto;*/
+.item {
+  padding: 16px 24px;
+  border-radius: 4px;
+  background: #f5f5f5;
+  margin-right: 24px;
+  box-sizing: border-box;
+  width: calc((100% - (3 * 24px)) / 4);
 }
 </style>
