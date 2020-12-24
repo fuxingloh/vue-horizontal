95ceda8082bbcc6afed496b617b03c64203e3859
Fuxing Loh <git@fuxing.dev>
2020-12-24 15:43:50 +0800

    fix: svg to fill current color so that it can be easily edited in CSS

--- src/vue-horizontal.vue
+++ src/vue-horizontal.vue
@@ -219,6 +219,8 @@ svg {
   border-radius: 21px;
   box-sizing: border-box;
   background: white;
+  color: black;
+  fill: currentColor;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 }
 
