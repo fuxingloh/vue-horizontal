387bb207cd5988eb883cfb47fadc0379624016da
Fuxing Loh <git@fuxing.dev>
2020-12-26 20:54:03 +0800

    chore: refactor slot to v-slot as former is deprecated

--- docs/content/snippets/features/features-nav-button-slot.vue
+++ docs/content/snippets/features/features-nav-button-slot.vue
@@ -1,16 +1,16 @@
 <template>
   <vue-horizontal>
     <component-example class="component" v-for="i in [1,2,3,4,5,6,7,8]" :key="i">
-      <h3>Override {{i}}</h3>
+      <h3>Override {{ i }}</h3>
     </component-example>
 
     <template v-slot:btn-prev>
       <button>Prev</button>
     </template>
 
-    <button slot="btn-next">
-      Next
-    </button>
+    <template v-slot:btn-next>
+      <button>Next</button>
+    </template>
   </vue-horizontal>
 </template>
 
