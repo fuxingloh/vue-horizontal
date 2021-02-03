e741832b43724337a64549fffc7f26f6db2aa29a
Fuxing Loh <git@fuxing.dev>
2021-02-03 15:13:59 +0800

    Moving style inline, removing responsive due to media query limitations

--- src/VueHorizontal.vue
+++ src/VueHorizontal.vue
@@ -1,30 +1,106 @@
 <template>
-  <div class="vue-horizontal">
+  <div class="vue-horizontal"
+       :style="{
+        'position': 'relative',
+        'display': 'flex',
+       }"
+  >
     <div class="v-hl-btn v-hl-btn-prev" v-if="button && hasPrev" @click.stop="prev" role="button"
-         :class="{'v-hl-btn-between': buttonBetween}">
+         :style="{
+          'left': 0,
+          'cursor': 'pointer',
+          'position': 'absolute',
+          'align-self': 'center',
+          'z-index': 1,
+          'top': 0,
+          'bottom': 0,
+          'display': 'flex',
+          'align-items': 'center',
+          'transform': buttonBetween ? 'translateX(-50%)': 'none',
+         }"
+    >
       <slot name="btn-prev">
-        <svg class="v-hl-svg" viewBox="0 0 24 24" aria-label="horizontal scroll area navigate to previous button">
+        <svg class="v-hl-svg" viewBox="0 0 24 24" aria-label="horizontal scroll area navigate to previous button"
+             :style="{
+              'width': '40px',
+              'height': '40px',
+              'margin': '6px',
+              'padding': '6px',
+              'border-radius': '20px',
+              'box-sizing': 'border-box',
+              'background': 'white',
+              'color': 'black',
+              'fill': 'currentColor',
+              'box-shadow': '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+             }"
+        >
           <path d="m9.8 12 5 5a1 1 0 1 1-1.4 1.4l-5.7-5.7a1 1 0 0 1 0-1.4l5.7-5.7a1 1 0 0 1 1.4 1.4l-5 5z"/>
         </svg>
       </slot>
     </div>
 
     <div class="v-hl-btn v-hl-btn-next" v-if="button && hasNext" @click.stop="next" role="button"
-         :class="{'v-hl-btn-between': buttonBetween}">
+         :style="{
+          'right': 0,
+          'cursor': 'pointer',
+          'position': 'absolute',
+          'align-self': 'center',
+          'z-index': 1,
+          'top': 0,
+          'bottom': 0,
+          'display': 'flex',
+          'align-items': 'center',
+          'transform': buttonBetween ? 'translateX(50%)': 'none',
+         }"
+    >
       <slot name="btn-next">
-        <svg class="v-hl-svg" viewBox="0 0 24 24" aria-label="horizontal scroll area navigate to next button">
+        <svg class="v-hl-svg" viewBox="0 0 24 24" aria-label="horizontal scroll area navigate to next button"
+             :style="{
+              'width': '40px',
+              'height': '40px',
+              'margin': '6px',
+              'padding': '6px',
+              'border-radius': '20px',
+              'box-sizing': 'border-box',
+              'background': 'white',
+              'color': 'black',
+              'fill': 'currentColor',
+              'box-shadow': '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+             }"
+        >
           <path d="m14.3 12.1-5-5a1 1 0 0 1 1.4-1.4l5.7 5.7a1 1 0 0 1 0 1.4l-5.7 5.7a1 1 0 0 1-1.4-1.4l5-5z"/>
         </svg>
       </slot>
     </div>
 
-    <div class="v-hl-container" ref="container" @scroll.passive="onScroll" :class="{
-      'v-hl-responsive': responsive,
-      'v-hl-scroll': scroll,
-      'v-hl-snap-start': snap === 'start',
-      'v-hl-snap-center': snap === 'center',
-      'v-hl-snap-end': snap === 'end',
-    }">
+    <div class="v-hl-container" ref="container" @scroll.passive="onScroll"
+         :class="{
+          'v-hl-scroll': scroll,
+          'v-hl-snap-start': snap === 'start',
+          'v-hl-snap-center': snap === 'center',
+          'v-hl-snap-end': snap === 'end',
+          }"
+         :style="{
+          'display': 'flex',
+          'width': '100%',
+          'margin': 0,
+          'padding': 0,
+          'border': 'none',
+          'box-sizing': 'content-box',
+          'overflow-x': 'scroll',
+          'overflow-y': 'hidden',
+          'scroll-snap-type': 'x mandatory',
+          '-webkit-overflow-scrolling': 'touch',
+
+          /* Hiding scrollbar */
+          'scrollbar-width': 'none',
+          '-ms-overflow-style': 'none',
+          /* To effectively hide scrollbar for iOS Safari. 10% of the users. */
+          'padding-bottom': '30px',
+          'margin-bottom': '-30px',
+          'clip-path': 'inset(0 0 30px 0)',
+         }"
+    >
       <slot></slot>
     </div>
   </div>
@@ -69,13 +145,6 @@ export default defineComponent({
      * Scrollbar visibility
      */
     scroll: {
-      type: Boolean,
-      default: () => false,
-    },
-    /**
-     * Use default responsive breakpoint.
-     */
-    responsive: {
       type: Boolean,
       default: () => false,
     },
@@ -262,67 +331,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.vue-horizontal {
-  position: relative;
-  display: flex;
-}
-
-.v-hl-btn {
-  position: absolute;
-  align-self: center;
-  z-index: 1;
-  top: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-}
-
-.v-hl-btn:hover {
-  cursor: pointer;
-}
-
-.v-hl-btn-prev {
-  left: 0;
-}
-
-.v-hl-btn-prev.v-hl-btn-between {
-  transform: translateX(-50%);
-}
-
-.v-hl-btn-next {
-  right: 0;
-}
-
-.v-hl-btn-next.v-hl-btn-between {
-  transform: translateX(50%);
-}
-
-.v-hl-svg {
-  width: 40px;
-  height: 40px;
-  margin: 6px;
-  padding: 6px;
-  border-radius: 20px;
-  box-sizing: border-box;
-  background: white;
-  color: black;
-  fill: currentColor;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-}
-
-.v-hl-container {
-  display: flex;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-  border: none;
-  box-sizing: content-box;
-  overflow-x: scroll;
-  overflow-y: hidden;
-  scroll-snap-type: x mandatory;
-  -webkit-overflow-scrolling: touch;
-}
-
 ::v-slotted(.v-hl-container > *) {
   flex-shrink: 0;
   box-sizing: border-box;
@@ -341,56 +349,5 @@ export default defineComponent({
 
 ::v-slotted(.v-hl-snap-end > *) {
   scroll-snap-align: end;
-}
-
-.v-hl-container:not(.v-hl-scroll) {
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-
-  /* To effectively hide scrollbar for iOS Safari. 10% of the users. */
-  padding-bottom: 30px;
-  margin-bottom: -30px;
-  clip-path: inset(0 0 30px 0);
-}
-
-.v-hl-container:not(.v-hl-scroll)::-webkit-scrollbar {
-  /* !important: So that users don't accidentally show scrollbar. */
-  width: 0 !important;
-  height: 0 !important;
-}
-
-/* Using https://tailwindcss.com/docs/responsive-design breakpoints. */
-
-::v-slotted(.v-hl-responsive > *) {
-  width: 100%;
-  margin-right: 24px;
-}
-
-::v-slotted(.v-hl-responsive > *:last-child) {
-  margin-right: 0;
-}
-
-@media (min-width: 640px) {
-  ::v-slotted(.v-hl-responsive > *) {
-    width: calc((100% - 24px) / 2);
-  }
-}
-
-@media (min-width: 768px) {
-  ::v-slotted(.v-hl-responsive > *) {
-    width: calc((100% - 48px) / 3);
-  }
-}
-
-@media (min-width: 1024px) {
-  ::v-slotted(.v-hl-responsive > *) {
-    width: calc((100% - 72px) / 4);
-  }
-}
-
-@media (min-width: 1280px) {
-  ::v-slotted(.v-hl-responsive > *) {
-    width: calc((100% - 96px) / 5);
-  }
 }
 </style>
